b5475ccc7f031091127be6f050d36e988ca28305
github username <github email address>
2018-02-03 14:50:31 -0500

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,43 +1,71 @@
 require 'pry'
 
 class Artist
+  #Spawn an artists, you can read and write their name
+  #You can only read their songs
   attr_accessor :name
   attr_reader :songs
 
+extend Memorable::ClassMethods
+include Memorable::InstanceMethods
+extend Findable
+include Paramable
   @@artists = []
+  #Store ALL of the spawned artists songs into this class array
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
+    #Look in the class array
+    #If the arguement passed matches the value (test passes)
+    #give me back that artist
+
 
   def initialize
-    @@artists << self
+    super
     @songs = []
+    #Every artists created will put their name into my class array
+    #and have their own array that holds a collection of songs
   end
+  #smells funky
 
   def self.all
     @@artists
+    #Call upon the class array with all the artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  #   #clear the artists array
+  # end
 
-  def self.count
-    @@artists.count
-  end
+  # def self.count
+  #   @@artists.count
+  #   #how many artists are there?
+  # end
 
   def add_song(song)
     @songs << song
     song.artist = self
+    #take in a song as an arguement
+    #add it to the artists song array
+    #the song knows where it came from
   end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
+
+
+    #take in a song as an arguement
+    #in the artists songs array
+
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+#   def to_param
+#     name.downcase.gsub(' ', '-')
+# #take the artists name
+# #downcase it
+# #then and replace spaces with a dash
+#   end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,33 +1,50 @@
 require 'pry'
 
 class Song
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
   attr_accessor :name
   attr_reader :artist
-
+  #everytime i create a song i can read and write its name
+  #but i can only read the artist name and cannot make any changes
   @@songs = []
+#keep an array of all the songs I create
 
   def initialize
-    @@songs << self
+    self.class.all << self
+    #everytime i make a song
+    #push that song into the class array songs
   end
+  #smells funky
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
+    #given a name
+    #look in my songs array
+    #find me the matching songs
+
 
   def artist=(artist)
     @artist = artist
+    #the artist name can be rewritten?
   end
 
   def self.reset_all
     @@songs.clear
+    #clear my class songs arrray
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  #   #take my song name, and replace spaces with dashes?
+  # end
 
   def self.count
     self.all.count
+    #count
   end
 
   def self.all
